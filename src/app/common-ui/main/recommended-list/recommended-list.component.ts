0b3f2f54cade056af294fd31cdc24f9ebf4fae80
YuriyLt <j.litvinovs@gmail.com>
2024-09-18 18:43:04 +0300

    template

--- src/app/common-ui/main/recommended-list/recommended-list.component.ts
+++ src/app/common-ui/main/recommended-list/recommended-list.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Casino, casinoes } from './recommended-casino-list.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-recommended-list',
   standalone: true,
-  imports: [],
-  templateUrl: './recommended-list.component.html',
-  styleUrl: './recommended-list.component.scss'
+  imports: [NgFor, CommonModule],
+  template: `
+  <article class="recommended">
+    <h3>Recommended Casinos</h3>
+    <ul>
+        <li *ngFor="let casino of casinoes">
+            <ul class="sublist">
+                <li><img [src]="casino.logo" alt="{{casino.name}}"></li>
+                <li>{{casino.name}}</li>
+                <li>{{casino.rating}}</li>
+                <li><img [src]="casino.starRating" alt="StarRating"></li>
+                <li>{{casino.desc}}</li>
+            </ul>
+        </li>
+      </ul>
+      <button class="rec-btn">See more</button>
+  </article>
+  `,
+
+  styleUrls: ['./recommended-list.component.scss']
 })
 export class RecommendedListComponent {
-
+  casinoes: Casino[] = casinoes;
 }
