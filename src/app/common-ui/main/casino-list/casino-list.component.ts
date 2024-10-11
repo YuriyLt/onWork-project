6795232132e311b03dc05a9e2f00597b66489f98
YuriyLt <j.litvinovs@gmail.com>
2024-10-11 12:48:35 +0300

    showMoreButton + bootstrapStyles

--- src/app/common-ui/main/casino-list/casino-list.component.ts
+++ src/app/common-ui/main/casino-list/casino-list.component.ts
@@ -1,67 +1,37 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Casino, casinoes } from '../../../casino';
+import { Payment, payments } from '../../../payment';
 
 @Component({
   selector: 'app-casino-list',
   standalone: true,
   imports: [CommonModule],
-  template: `
-  <h2 class='mainSectionHeading'>Top Casinos</h2>
-  <div class="position-container">
-    <div class="container" *ngFor="let casino of casinoes">
-    <div class="casinoType">
-      <div class="casino-container">
-        <div class="logo"><img [src]="casino.logo"></div>
-        <div class="ratings">
-        <div class="rating">{{casino.rating}}</div>
-        <div class="starRating"><img width="70px" height="13px" [src]="casino.starRating"></div>
-        </div>
-        <div class="buttonVisit"><button><a [href]="casino.link">Visit casino</a></button></div>
-        </div>
-      </div>
-      <div class="aboutCasino">
-        <h2>{{casino.aboutHeading}}</h2>
-        <ul>
-          <li>
-            <img width="17px" height="18px" src="../../assets/bonuss.svg" alt="">
-            <span>{{casino.aboutOptionsBonuss}}</span>
-          </li>
-          <li>
-            <img width="17.5px" height="17.5px" src="../../assets/clock.svg" alt="">
-            <span>{{casino.aboutOptionsWithdraw}}</span>
-          </li>
-          <li>
-            <img width="20px" height="20px" src="../../assets/withdraw.svg" alt="">
-            <span>{{casino.aboutOptionsDaily}}</span>
-          </li>
-          <li>
-            <img width="16px" height="16px" src="../../assets/chat.svg" alt="">
-            <span>{{casino.aboutOptionsChat}}</span>
-          </li>
-        </ul>
-      </div>
-      <div class="paymentSection">
-        <div class="d-flex align-items-center justify-content-between w-100">
-          <h2 class="text-white">{{casino.paymentHeading}}</h2>
-          <a href="" class="paymentSection-showmore">Show more</a>
-        </div>
-        
-        <ul class="paymentMethods">
-          <li><img [src]="casino.paymentRapid" alt="Rapid" class="paymentBank"></li>
-          <li><img [src]="casino.paymentRevolut" alt="Revolut" class="paymentBank"></li>
-          <li><img [src]="casino.paymentAstropay" alt="Astropay" class="paymentBank"></li>
-          <li><img [src]="casino.paymentDE" alt="DepositExpress" class="paymentBank"></li>
-          <li><img [src]="casino.paymentCitadele" alt="Citadele" class="paymentBank"></li>
-          <li><img [src]="casino.paymentBitcoin" alt="Bitcoin" class="paymentBank"></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-`,
-  
-  styleUrls: ['./casino-list.component.scss']
+  templateUrl: './casino-list.component.html',
+  styleUrls: [
+    './casino-list.component.scss',
+  ]
 })
 export class CasinoListComponent {
-  casinoes: Casino[] = casinoes;
+  public casinoes: Casino[] = casinoes;
+  public payments: Payment[] = payments;
+  public shownPayments = [...this.payments];
+  public showMore: boolean = false;
+
+  ngOnInit() {
+    this.updatePaymentList();
+  }
+
+  private updatePaymentList():void {
+    if (!this.showMore) {
+      this.shownPayments = this.shownPayments.slice(0, 6);
+    } else {
+      this.shownPayments = [...this.payments];
+    }
+  }
+
+  allPayments(): void {
+    this.showMore = !this.showMore;
+    this.updatePaymentList();
+  }
 }
---
+++ src/app/payment.ts
@@ -0,0 +1,55 @@
+export interface Payment {
+    name: string;
+    logo: string;
+}
+
+export const payments: Payment[] = [
+   {
+    name: 'Revolut',
+    logo: '../assets/revolut.svg',
+   },
+   {
+    name: 'Rapid',
+    logo: '../assets/rapid.svg',
+   },
+   {
+    name: 'Astropay',
+    logo: '../assets/astropay_payment.svg',
+   },
+   {
+    name: 'DepositExpress',
+    logo: '../assets/deposit-express.svg',
+   },
+   {
+    name: 'Citadele',
+    logo: '../assets/citadele.svg',
+   },
+   {
+    name: 'Bitcoin',
+    logo: '../assets/bitcoin.svg'
+   },
+   {
+    name: 'ecoPayz', 
+    logo: '../assets/ecopayz.svg',
+   },
+   {
+    name: 'Jeton',
+    logo: '../assets/jeton.svg',
+   },
+   {
+    name: 'Klarna.',
+    logo: '../assets/klarna.svg',
+   },
+   {
+    name: 'mastercard',
+    logo: '../assets/mastercard.svg',
+   },
+   {
+    name: 'N26',
+    logo: '../assets/n26.svg',
+   },
+   {
+    name: 'Neosurf',
+    logo: '../assets/neosurf.svg',
+   }
+]
